e18ca5966cd5c2a4cf54577175b55da5aa43114a
topwdo777 <archieturner2024@gmail.com>
2026-04-13 09:35:29 +0100

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,9 +25,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "Qit's DonutMarket Place! Best Prices and Easiest Invite Rewards! https://discord.gg/NBBmfM5NqX ",
         // Activity type number (0 = Playing).
-        type: 0, 
+        type: 5, 
       },
     ],
   },
@@ -88,8 +88,8 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699", 
-      secondary: "#2F3136", 
+      primary: "#228B22", 
+      secondary: "#0A0A0A", 
 
       // Standard status colors for success/error/warning/info messages.
       success: "#57F287", 
